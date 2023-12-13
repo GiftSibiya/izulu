839bc5ffbad8ebfbecdf156b8e779fb06013e42d
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-13 20:51:22 +0200

    app layout

--- izulu_v1/lib/src/common/main_weather.dart
+++ izulu_v1/lib/src/common/main_weather.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:izulu_v1/src/constants/colors.dart';
+import 'package:izulu_v1/src/constants/image_strings.dart';
 
 class mainWeather extends StatelessWidget {
   const mainWeather({
@@ -12,13 +13,56 @@ class mainWeather extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Container(
         height: MediaQuery.of(context).size.height * 0.5,
+        width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-            color: bgLightGrey, borderRadius: BorderRadius.circular(20)),
+          color: bgMidGrey,
+          borderRadius: BorderRadius.circular(20),
+        ),
         child: Column(
           children: [
-//-- THe first stuff for today--//
-
+//--Today's Weather--//
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: bgLightGrey,
+                    borderRadius: BorderRadius.circular(20)),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Text("Today's Weather"),
+                        Text(
+                          "Wind:   25KM/H",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          "Humidity:   16%",
+                          style: TextStyle(fontSize: 12),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          child: Image(
+                            image: AssetImage(cloudy),
+                          ),
+                        ),
+                        Text("Partly Cloudy")
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
 //--//
+            Container(
+              color: bgLightGrey,
+              child: Text("Today's Forcast"),
+            )
           ],
         ),
       ),
---
+++ izulu_v1/lib/src/constants/image_strings.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+//--WEATHER STATUS ICONS --//
+
+const cloudy = "assets/weather_icons/cloudy.png";
