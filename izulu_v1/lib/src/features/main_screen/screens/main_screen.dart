191c5b0c7ad73bd533af9846c37ea0a446d31d61
Gift Sibiya <giftsibiya.gb@gmail.com>
2024-01-30 22:40:03 +0200

    Update main_screen.dart

--- izulu_v1/lib/src/features/main_screen/screens/main_screen.dart
+++ izulu_v1/lib/src/features/main_screen/screens/main_screen.dart
@@ -1,7 +1,13 @@
+/// DEPENDENCY IMPORTS ///
 import 'package:flutter/material.dart';
+
+///--///
+
+/// FILE IMPORTS ///
 import 'package:izulu_v1/src/common/app_header.dart';
 import 'package:izulu_v1/src/common/main_weather.dart';
 import 'package:izulu_v1/src/constants/colors.dart';
+///--///
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
